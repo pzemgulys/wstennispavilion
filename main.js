aba58fe9945a67f3a099a68cda143b4aa6f4abc4
pzemgulys <paul@zemgulys.com>
2019-02-17 17:03:35 -0600

    Add files via upload
    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var app_browser_module_1 = require(".././src/app/app.browser.module");
+var core_1 = require("@angular/core");
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+require("hammerjs");
+var environment_1 = require("./environments/environment");
+if (environment_1.environment.production) {
+    core_1.enableProdMode();
+}
+platform_browser_dynamic_1.platformBrowserDynamic()
+    .bootstrapModule(app_browser_module_1.AppBrowserModule)
+    .catch(function (err) { return console.log(err); });
+//# sourceMappingURL=main.js.map
